aab45db19f6251a5c60f31db8dff410b81dd5b26
GyowonS <sgyowon0527@gmail.com>
2026-04-24 10:22:24 -0600

    master.c for gyro x-axis control check

--- master/Src/master.c
+++ master/Src/master.c
@@ -29,12 +29,11 @@ int main(void)
   /* Configure the system clock */
   SystemClock_Config();
 
+  SetupLEDs();
+  i2c_init();
 
   motor_init();
   gyro_init();
-
-  SetupLEDs();
-  i2c_init();
 
   // For debugging
   SetupGPIO_USART();
@@ -46,26 +45,33 @@ int main(void)
   //i2c_setup function shifts adress already
   uint8_t device_address = (0x10);   // STM32 slave device address
   char* data = "Hello from master device";
+  int16_t threshold = 250;
 
   while (1)
   {
+    I2C2->CR1 &= ~(I2C_CR1_TXIE | I2C_CR1_RXIE | I2C_CR1_TCIE | I2C_CR1_NACKIE);
     // Read raw X-axis velocity from gyroscope
-    int16_t x_axis_rate = gyro_readX();
-    target_position = x_axis_rate;
+    int16_t x_val = gyro_readX();
+    // Enable I2C interrupt for slave communication
+    I2C2->CR1 |= (I2C_CR1_TXIE | I2C_CR1_RXIE | I2C_CR1_TCIE | I2C_CR1_NACKIE);
+    // Velocity to position
+    if (x_val > threshold || x_val < -threshold) {
+      target_position += (x_val / 16);
+    }
+    HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6 | GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9, GPIO_PIN_RESET);
 
-    HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6 | GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9, GPIO_PIN_SET);
-
+    if (x_val > threshold) {
+      HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, GPIO_PIN_SET);
+    }
+    else if (x_val < -threshold) {
+      HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, GPIO_PIN_SET);
+    }
     I2C_Write(I2C2, device_address, strlen(data) + 1, data);
 
     while (I2C_ongoingTransaction) {
       // Spin loop
     }
-
-    HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, GPIO_PIN_SET);
-
-    I2C_Write(I2C2, device_address, strlen(data) + 1, data);
-
-    HAL_Delay(500); // Give time for slave's interrupt handler
+    HAL_Delay(50); // Give time for slave's interrupt handler
   }
   return -1;
 }
